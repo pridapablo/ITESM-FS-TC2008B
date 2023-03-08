b643717400cad6fd67f420bf7621299629d07475
Pablo Banzo <pridapablo@gmail.com>
2023-03-08 14:03:00 -0600

    working score and game over

--- Videogames/MyProject/Assets/Scripts/CrossyRoad/EnemyCollision.cs
+++ Videogames/MyProject/Assets/Scripts/CrossyRoad/EnemyCollision.cs
@@ -1,22 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyCollision : MonoBehaviour
 {
+    [SerializeField] TMPro.TextMeshProUGUI scoreText;
+    public int score = 0;
+    // Origin Vector
+    Vector3 origin = new Vector3(0, -6, 0);
+
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // when the ball enters the hoop collider, trigger the "Score" function
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Slay");
-        if (collision.gameObject.tag == "Enemy")
+        if (other.gameObject.tag == "Enemy")
         {
             Debug.Log("Collision with enemy");
+            // Score Text to Game Over
+            scoreText.text = "Game Over";
+            score = 0;
+            // reset the player to the start
+            gameObject.transform.position = origin;
+        }
+        if (other.gameObject.tag == "Finish")
+        {
+            score++;
+            scoreText.text = "Score: " + score;
+            // reset the player to the start
+            gameObject.transform.position = origin;
         }
     }
 }
